176cf51c110e2d023d02e9506a3d6c673bd1ee8a
ganesh-dagadi <ganeshdagadi3@gmail.com>
2022-01-22 15:59:05 +0530

    Added client domain for cors

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const app = express()
 
 mongoose.connect(process.env.MONGO_URI)
 
-app.use(cors())
+app.use(cors(
+    {
+        origin : 'http://time-trac.vercel.app'
+    }
+))
 app.use(json())
 app.use(urlencoded({extended : false}));
 
